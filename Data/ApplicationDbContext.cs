54fb503447410c02cc8d624b8a43efffc1348931
Seth Dana <seth.dana@gmail.com>
2018-10-24 14:58:44 -0500

    Initial commit. Ready to add models for DB tables, add migration and create DB

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using ChattrApi.Models;
+
+namespace ChattrApi.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<User> User { get; set; }
+
+    }
+}
